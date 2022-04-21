f15c474050b4b0511edd7f3b3f7cc5f8e413df10
Ben Merchant <benjaminmerchant@gmail.com>
2022-04-21 14:51:41 -0400

    first commit

---
+++ strings.py
@@ -0,0 +1,9 @@
+# Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
+name = 'Benjo'
+
+# Concatenate
+print('Hello ' + name)
+
+# String Formatting
+
+# String Methods
